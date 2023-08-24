74148bbe572a53e385c28fc9eb2a0f7902818bdc
moerjie <3165109406@qq.com>
2023-08-24 21:02:18 +0800

    8.24

--- 448.找到所有数组中消失的数字.py
+++ 448.找到所有数组中消失的数字.py
@@ -1,3 +1,9 @@
+'''
+Author: Moerjie
+Data: Today
+LastEditTime: 2023-08-24 21:01:28
+FilePath: \python\448.找到所有数组中消失的数字.py
+'''
 #
 # @lc app=leetcode.cn id=448 lang=python3
 #
@@ -5,7 +11,17 @@
 #
 
 # @lc code=start
+
+
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-# @lc code=end
+        count = [0]*(len(nums)+1)
+        for num in nums:
+            count[num] += 1
+        res = []
+        for num in range(1, len(nums)+1):
+            if count[num] == 0:
+                res.append(num)
+        return res
 
+# @lc code=end
