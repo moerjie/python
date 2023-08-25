f380ee33f6c9d0dec40cdf7cb1da180909ac3d7b
moerjie <3165109406@qq.com>
2023-08-25 21:22:01 +0800

    8.25

---
+++ 709.转换成小写字母.py
@@ -0,0 +1,19 @@
+'''
+Author: Moerjie
+Data: Today
+LastEditTime: 2023-08-24 21:35:10
+FilePath: \python\709.转换成小写字母.py
+'''
+#
+# @lc app=leetcode.cn id=709 lang=python3
+#
+# [709] 转换成小写字母
+#
+
+# @lc code=start
+
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+# @lc code=end
