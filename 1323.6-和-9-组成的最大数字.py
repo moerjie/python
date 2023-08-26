4ffa03ea2ec87b3006d576bf0af3af5314ff3995
moerjie <3165109406@qq.com>
2023-08-26 11:11:41 +0800

    8.26

--- 1323.6-和-9-组成的最大数字.py
+++ 1323.6-和-9-组成的最大数字.py
@@ -1,3 +1,9 @@
+'''
+Author: Moerjie
+Data: Today
+LastEditTime: 2023-08-26 11:07:47
+FilePath: \python\1323.6-和-9-组成的最大数字.py
+'''
 #
 # @lc app=leetcode.cn id=1323 lang=python3
 #
@@ -5,7 +11,9 @@
 #
 
 # @lc code=start
-class Solution:
-    def maximum69Number (self, num: int) -> int:
-# @lc code=end
 
+
+class Solution:
+    def maximum69Number(self, num: int) -> int:
+        return int(str(num).replace('6', '9', 1))
+# @lc code=end
---
+++ Notes.md
@@ -0,0 +1,8 @@
+# Note
+
+## Function
+
+### replace()
+
+str.replace(old,new,max)
+替换不超过max次
