bde17b45e6d1608e6b00fca3a614d531215d3349
moerjie <3165109406@qq.com>
2023-08-30 20:56:55 +0800

    add mul_walk

--- data_show/rw_visual.py
+++ data_show/rw_visual.py
@@ -1,13 +1,18 @@
 '''
 Author: Moerjie
 Data: Do not edit
-LastEditTime: 2023-08-30 20:45:16
+LastEditTime: 2023-08-30 20:50:28
 FilePath: \python\data_show\rw_visual.py
 '''
 import matplotlib.pyplot as plt
 from rand import Randomwalk
 
-rw=Randomwalk()
-rw.fill_walk()
-plt.scatter(rw.x_values,rw.y_values,s=15)
-plt.show()
+while True:
+    rw=Randomwalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_values,rw.y_values,s=15)
+    plt.show()
+
+    keep_running=input("Make another walk?(y/n): ")
+    if keep_running =='n':
+        break
