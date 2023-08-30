bd5015e727f9347e482cb3fe9d29266c28750164
moerjie <3165109406@qq.com>
2023-08-30 22:16:09 +0800

    test

---
+++ data_show/1.py
@@ -0,0 +1,9 @@
+'''
+Author: Moerjie
+Data: Do not edit
+LastEditTime: 2023-08-30 21:09:34
+FilePath: \python\data_show\1.py
+'''
+a = input("input a num: ")
+
+print(a)
--- data_show/rw_visual.py
+++ data_show/rw_visual.py
@@ -1,18 +1,25 @@
 '''
 Author: Moerjie
 Data: Do not edit
-LastEditTime: 2023-08-30 20:50:28
+LastEditTime: 2023-08-30 21:30:36
 FilePath: \python\data_show\rw_visual.py
 '''
 import matplotlib.pyplot as plt
 from rand import Randomwalk
 
+
 while True:
-    rw=Randomwalk()
+    rw = Randomwalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values,rw.y_values,s=15)
+
+    point_num = list(range(rw.num_points))
+
+    plt.scatter(rw.x_values, rw.y_values, c=point_num,
+                cmap=plt.cm.Blues, edgecolors='none', s=10)
+    plt.scatter(0, 0, c='red', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', s=100)
     plt.show()
 
-    keep_running=input("Make another walk?(y/n): ")
-    if keep_running =='n':
+    keep_running = input("Make another walk?(1/2): ")
+    if keep_running == '1':
         break
