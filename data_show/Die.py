9e6268e374430f753171d2bafdb2d31688a3d458
moerjie <3165109406@qq.com>
2023-08-31 18:50:29 +0800

    8.31

--- data_show/Die.py
+++ data_show/Die.py
@@ -1,10 +1,25 @@
+'''
+Author: Moerjie
+Data: Do not edit
+LastEditTime: 2023-08-31 18:22:13
+FilePath: \data_show\Die.py
+'''
 from random import randint
 
+
 class Die():
-    def __init__(self,num_sides=6):
-        self.num_sides=num_sides
+    def __init__(self, num_sides=8):
+        self.num_sides = num_sides
 
     def roll(self):
-        return  randint(1,self.num_sides)
+        return randint(1, self.num_sides)
 
-    
+    def creat_x(self):
+        x_label = []
+        i = j = 1
+        for i in range(1, self.num_sides+1):
+            for j in range(1, self.num_sides+1):
+                x_label.append(i+j)
+                j += 1
+            i += 1
+        return x_label
--- data_show/die_6_mul.py
+++ data_show/die_6_mul.py
@@ -1,24 +1,24 @@
 '''
 Author: Moerjie
 Data: Do not edit
-LastEditTime: 2023-08-31 18:34:26
-FilePath: \data_show\die_6_visual.py
+LastEditTime: 2023-08-31 18:47:52
+FilePath: \data_show\die_6_mul.py
 '''
 from Die import Die
 import pygal
 
 die_1 = Die()
 die_2 = Die()
-die_3 = Die()
+
 
 results = []
 frequencies = []
-max_result = die_1.num_sides + die_2.num_sides+die_3.num_sides
+max_result = die_1.num_sides * die_2.num_sides
 for roll_num in range(1000000):
-    result = die_1.roll() + die_2.roll()+die_3.roll()
+    result = die_1.roll() * die_2.roll()
     results.append(result)
 
-for value in range(3, max_result + 1):
+for value in range(1, max_result + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
@@ -29,6 +29,6 @@ hist = pygal.Bar()
 
 hist.title = "Results of rolling two D6 1000 times"
 # hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
-hist.x_labels = [str(x) for x in range(2, max_result+1)]
+hist.x_labels = [str(x) for x in range(1, max_result+1)]
 hist.add('D6+D6+D6', frequencies)
-hist.render_to_file('die_visual.svg')
+hist.render_to_file('die_visual_mul.svg')
