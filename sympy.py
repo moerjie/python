c0015fc3d141f9faf318d991f71624497eabcbcb
Moerjie <sawenmoerjie@outlook.com>
2023-09-03 19:57:24 +0800

    9.3

--- sympy.py
+++ sympy.py
@@ -1,11 +1,13 @@
 '''
 Author: Moerjie
 Data: Do not edit
-LastEditTime: 2023-09-01 15:39:03
+LastEditTime: 2023-09-03 19:56:06
 FilePath: \python\sympy.py
 '''
 from sympy import *
+from matplotlib import pyplot as plt
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+y = [x**2 for x in x]
+plt.scatter(x, y)
 
-x = Symbol("x")
-
-print(x)
+plt.show()
